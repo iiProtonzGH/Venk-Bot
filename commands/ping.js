3bc91440750356c93852bb2966009e5ae9ab00bf
VenkSociety <VenkSociety@users.noreply.github.com>
2018-02-26 16:17:36 +1000

    Change "xms" to "loading..." whilst waiting for ping to calculate.

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message) {
-		message.channel.send("xms").then(msg => msg.edit(msg.createdTimestamp - message.createdTimestamp + "ms"));
+		message.channel.send("loading...").then(msg => msg.edit(msg.createdTimestamp - message.createdTimestamp + "ms"));
 	},
 };
