72ca65b87dc19614764d35756e6c91ec869d9563
VenkSociety <VenkSociety@users.noreply.github.com>
2018-02-26 16:22:04 +1000

    Notify user that the bot has sent them a message if they haven't seen otherwise.

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'help',
 	description: 'Helpful stuff.',
 	execute(message) {
+		message.channel.send(`I've sent you a message ${message.author.username}, please check your DMs!`);
 		message.author.send('A list of commands can be found by doing **/commands**.\n \n Want to add me to your server?\n https://discordapp.com/api/oauth2/authorize?client_id=379500653577371649&permissions=8&scope=bot \n \n Still need more help? Why not head on over to our community Discord? \n https://discord.gg/KcpEXTK'); 
 	},
 };
