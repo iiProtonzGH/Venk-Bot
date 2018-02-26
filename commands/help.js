6c6efcbdba9160f16430e39561cddc2b6783d5d6
VenkSociety <VenkSociety@users.noreply.github.com>
2018-02-26 16:18:57 +1000

    Give bot its own "Venk Bot" role.

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'Helpful stuff.',
 	execute(message) {
-		message.author.send('A list of commands can be found by doing **/commands**.\n \n Want to add me to your server?\n https://discordapp.com/oauth2/authorize?client_id=379500653577371649&scope=bot \n \n Still need more help? Why not head on over to our community Discord? \n https://discord.gg/KcpEXTK'); 
+		message.author.send('A list of commands can be found by doing **/commands**.\n \n Want to add me to your server?\n https://discordapp.com/api/oauth2/authorize?client_id=379500653577371649&permissions=8&scope=bot \n \n Still need more help? Why not head on over to our community Discord? \n https://discord.gg/KcpEXTK'); 
 	},
 };
