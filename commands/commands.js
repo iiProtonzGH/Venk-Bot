c108603efa51cfb8a52accda099a612995191816
VenkSociety <VenkSociety@users.noreply.github.com>
2018-03-31 10:27:29 +1000

    Create rate.js
    Also documented/fixed grammar with /server.

--- commands/commands.js
+++ commands/commands.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Displays a list of commands.',
 	execute(message) {
 		message.channel.send(`I've sent you a message ${message.author.username}, please check your DMs!`);
-		message.author.send('**Venk Bot commands:**\n \n `/avatar` - Displays a URL to your profile picture.\n `/beep` - Boop.\n `/commands` - Displays this command list.\n `/help` - Information on how to setup/configure the bot as well as a support Discord link.\n `/ping` - Your current connection to the bot in miliseconds.\n `/prune` - Prunes up to 99 messages.\n `/whois` - Information about your account.'); 
+		message.author.send('**Venk Bot commands:**\n \n `/avatar` - Displays a URL to your profile picture.\n `/beep` - Boop.\n `/commands` - Displays this command list.\n `/help` - Information on how to setup/configure the bot as well as a support Discord link.\n `/ping` - Your current connection to the bot in miliseconds.\n `/prune` - Prunes up to 99 messages.\n `/rate` - Reacts with a thumbs up/thumbs down emoji.`/server` - Displays info about this server.\n `/whois` - Information about your account.'); 
 	},
 };
