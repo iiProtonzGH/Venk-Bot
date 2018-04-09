8dd7de50172b3b74ccea751e74d0a555d01e4ccf
VenkSociety <VenkSociety@users.noreply.github.com>
2018-04-09 10:42:21 +1000

    Add more info to /server

--- commands/server.js
+++ commands/server.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'server',
 	description: 'Displays info about this server.',
 	execute(message) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+		message.channel.send(`**Showing statistics for ${message.guild.name}**\nServer name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nServer ID: ${message.guild.id}\nCreated by: ${message.guild.owner} on ${message.guild.createdAt}\nAFK Channel: ${message.guild.afkChannel}\nAFK Timeout: ${message.guild.afkTimeout} seconds.`);
 	},
 };
